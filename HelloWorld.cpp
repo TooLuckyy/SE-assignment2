ed71007bf3fd12e7e0525426fb323b618bb6741d
Darrel Mosley <darrelmosley@mac.pv.pvpanther.net>
2026-03-19 12:44:09 -0500

    Update: message in main

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,11 +3,5 @@
 
 int main(){
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hi! Darrel" << std::endl;
-
-    int x = 1;
-    int y = 2;
-
-    int add = x + y;
-    std::cout << add << std::endl;
+    std::cout << "Hi Darrel, Today is Thursday." << std::endl;
 }
