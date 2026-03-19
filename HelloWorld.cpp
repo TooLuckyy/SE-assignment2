5228dd38dfc471396f012f03f1cd4639ac8c0b6f
Darrel Mosley <darrelmosley@mac.pv.pvpanther.net>
2026-03-19 12:55:49 -0500

    Resolved merge conflict

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,6 @@
 
 int main(){
     std::cout << "Hello, World!" << std::endl;
-    
+    std::cout << "Hi Darrel, Today is Thursday." << std::endl;
     std::cout << "What is your name?" << std::endl;
 }
